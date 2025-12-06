47ab67246d4006ce11555418ec20d6a46d90ef0d
aozodbek2197 <aozodbek2197@gmail.com>
2025-12-06 15:07:55 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,32 @@
+# # 1-mashq
+# roy = [7, 2, 4, 9, 5]
+
+# kub_kotar = list(map(lambda x: x**3, roy))
+# print(kub_kotar)
+# # 2-mashq
+# roy = ['Sam', 'Alex', 'Lisan', 'Obby', 'Michael']
+
+# birinchi_harf = list(map(lambda x: x[0], roy))
+# print(birinchi_harf)
+# # 3-mashq
+# roy = [7, 18, 3, 9, 4, 8]
+
+# juft_toq = list(map(lambda x: "Juft" if x % 2 == 0 else "Toq", roy))
+# print(juft_toq)
+# # 4-mashq
+# roy1 = [21, 6, 3, 9, 14]
+# roy2 = [2, 9, 23, 10, 5]
+
+# kopaytirish = list(map(lambda x, y: x * y, roy1, roy2))
+# print(kopaytirish)
+# # 5-mashq
+# matnlar = ["Salom123!", "Hello2025", 'Python!!!', "123,456!"]
+
+# natija = list(map(lambda s: ''.join(filter(str.isalpha, s)), matnlar))
+# print(natija)
+# # 6-mashq
+# sonlar = list(map(int, input("Sonlarni kiriting: ").split()))
+# daraja = int(input("Darajani kiriting: "))
+
+# natija = list(map(lambda x: x ** daraja, sonlar))
+# print(natija)
